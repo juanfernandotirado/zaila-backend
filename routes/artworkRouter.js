6c82bbb81eed0d6fe3b0aca00d12a94891e1123d
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-04 17:35:04 -0800

    Create Endpoints for GET Artwork

--- routes/artworkRouter.js
+++ routes/artworkRouter.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const artworkController = require("../controllers/artworkController.js");
 
-
+// This is to handle:
+// Retrieve all artework. example:  /api/artwork/
+// Retrieve one artework by sensorID. example:  /api/artwork/?sensorId=12345ABCDE&language=en-CA
 router.get('/', artworkController.getAllArtwork);
+
+// This is to handle:
+// Retrieve One artwork (path variable id). Example: /api/artwork/:artworkId
+// Retrieve one artework by sensorID. example:  /api/artwork/:artworkId/?language=en-CA
+router.get('/:artworkId', artworkController.getArtworkById);
 
 router.post('/', artworkController.createArtwork)
 
