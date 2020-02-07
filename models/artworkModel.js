4041bd353ff117f030bab5cc9bb0ca9f55ed19e4
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-06 20:22:08 -0800

    Create Artwork v1

--- models/artworkModel.js
+++ models/artworkModel.js
@@ -10,14 +10,42 @@ exports.getAllArtwork = (artworkId) => {
 }
 
 exports.createArtwork = (artwork) => {
-//     const newArtwork = new Artwork({
-//         title: artwork.title,
-//         author: artwork.author,
-//         description: artwork.description
-//     })
+
+    var options = {sql: `INSERT INTO artwork
+            (exhibitionId, 
+            sensorId, 
+            title, 
+            imageURL, 
+            artistName, 
+            media, 
+            year) 
+    VALUES (${cp.escape(artwork.exhibitionId)}, 
+            ${cp.escape(artwork.sensorId)}, 
+            ${cp.escape(artwork.title)},
+            ${cp.escape(artwork.imageURL)}, 
+            ${cp.escape(artwork.artistName)}, 
+            ${cp.escape(artwork.media)}, 
+            ${cp.escape(artwork.year)})`};
     
-//    return newArtwork.save();
-    return null;
-        
+    console.log("sql insert artwork: "+ options.sql)
+    return query(cp, options)
 }
 
+exports.createArtworkDetails = (artworkDetails, artworkId) => {
+
+    var inserts = '';
+    artworkDetails.forEach(detail => {
+        inserts += `INSERT INTO artworkDetails 
+            (artworkId, 
+            description, 
+            languageCode) 
+        VALUES (${cp.escape(artworkId)}, 
+                ${cp.escape(detail.description)}, 
+                ${cp.escape(detail.languageCode)});`
+    });
+
+    let options = {sql: inserts};
+    
+    console.log("sql insert artwork Detail: "+ options.sql)
+    return query(cp, options)
+}
