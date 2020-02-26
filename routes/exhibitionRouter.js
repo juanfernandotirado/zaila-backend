6b6ff2f19bb9279fe0f3873f9a62bcf96a466ac4
Juan_Tirado <50636966+juanfernandotirado@users.noreply.github.com>
2020-02-25 18:32:52 -0800

    Exhibition categories endpoint and exhibition progress

--- routes/exhibitionRouter.js
+++ routes/exhibitionRouter.js
@@ -8,6 +8,8 @@ const artworkController = require("../controllers/artworkController.js");
 
 router.get('/', exhibitionController.getAllExhibition);
 
+router.get('/category', exhibitionController.exhibitionCategory);
+
 router.get('/:exhibitionId',exhibitionController.getAllExhibition);
 
 router.get('/:exhibitionId/artwork', artworkController.getAllArtwork);
@@ -15,6 +17,8 @@ router.get('/:exhibitionId/artwork', artworkController.getAllArtwork);
 router.post('/', exhibitionController.validate('createExhibition'),exhibitionController.createExhibition);
 
 router.put('/:exhibitionId', exhibitionController.validate('updateExhibition'),exhibitionController.updateExhibition);
+
+
 
 
 exports.exhibitionRouter = router;
